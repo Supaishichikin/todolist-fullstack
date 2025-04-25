568ff84e303da7623a26465f5fb2619fceec1b9f
JordanCeres <jordan.toko@outlook.fr>
2025-04-25 20:20:15 +0200

    Task form component

--- frontend/src/components/form/FormTodo.tsx
+++ frontend/src/components/form/FormTodo.tsx
@@ -1,9 +1,43 @@
 import { Form} from "react-bootstrap"
+import { UpdateTodo } from "../../types"
+import { useState } from "react";
 
-export default function FormTodo(){
+export default function FormTodo(props:{new:boolean, values:UpdateTodo}){
+
+    const [title, setTitle] = useState(props.values?.title??"");
+    const [description, setDescription] = useState(props.values?.description??"");
+    const [to_complete_at, setToCompleteAt] = useState(props.values?.to_complete_at?
+    props.values?.to_complete_at.toISOString().split('T')[0]:new Date().toISOString().split('T')[0]);
+    const [completed, setCompleted] = useState(props.values?.completed??false);
+
     return <>
         <Form>
-            
+            <Form.Group className="mb-3" controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter title" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Enter description" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="to_complete_at">
+                <Form.Label>To complete at</Form.Label>
+                <Form.Control type="date" 
+                value={to_complete_at}
+                onChange={(e) => setToCompleteAt(e.target.value)    } />
+            </Form.Group>
+            {!props.new &&
+            <Form.Group className="mb-3" controlId="completed">
+                <Form.Label>Completed</Form.Label>
+                <Form.Check type="checkbox" label="Completed"
+                checked={completed}
+                onChange={(e) => setCompleted(e.target.checked)} />
+            </Form.Group>
+            }
         </Form>
     </>
 }
--- frontend/src/components/modals/TaskModals.tsx
+++ frontend/src/components/modals/TaskModals.tsx
@@ -1,48 +1,16 @@
-import { Form, Modal, Button } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import { UpdateTodo } from "../../types";
-import { useState } from "react";
+import FormTodo from "../form/FormTodo";
 
 export default function TaskModals(props:{new:boolean, show:boolean, setShow:Function, id?:string, values?:UpdateTodo}){
-    const [title, setTitle] = useState(props.values?.title??"");
-    const [description, setDescription] = useState(props.values?.description??"");
-    const [to_complete_at, setToCompleteAt] = useState(props.values?.to_complete_at?
-    props.values?.to_complete_at.toISOString().split('T')[0]:new Date().toISOString().split('T')[0]);
-    const [completed, setCompleted] = useState(props.values?.completed??false);
-
     return <>
         <Modal show={props.show} onHide={() => props.setShow(false)}>
             <Modal.Header closeButton>
                 <Modal.Title>{props.new ? "New Task" : "Update Task"}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="title">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Enter title" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="description">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control type="text" value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Enter description" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="to_complete_at">
-                        <Form.Label>To complete at</Form.Label>
-                        <Form.Control type="date" 
-                        value={to_complete_at}
-                        onChange={(e) => setToCompleteAt(e.target.value)    } />
-                    </Form.Group>
-                    {!props.new &&
-                    <Form.Group className="mb-3" controlId="completed">
-                        <Form.Label>Completed</Form.Label>
-                        <Form.Check type="checkbox" label="Completed"
-                        checked={completed}
-                        onChange={(e) => setCompleted(e.target.checked)} />
-                    </Form.Group>
-                    }
-                </Form>
+                <FormTodo new={props.new} values={props.values??
+                {title:"", description:"", to_complete_at:new Date(), completed:false}} />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={() => props.setShow(false)}>Cancel</Button>
